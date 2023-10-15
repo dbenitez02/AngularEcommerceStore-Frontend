4fb39bf9f498a011c9aa71dc0deca0545a98d934
dbenitez02 <36527173+D-Benz@users.noreply.github.com>
2023-10-15 13:37:01 -0700

    added ProductDetails componenet

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -14,24 +14,52 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) { }
 
+  /**
+   * Grab entire listing based on categoryId.
+   * @param theCategoryId 
+   * @returns 
+   */
   getProductList(theCategoryId: number): Observable<Product[]> {
     const serachUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
     return this.getProducts(serachUrl);
   
   }
-  
-  getProductCategories(): Observable<ProductCategory[]> {
 
+  /**
+   * Grabs list of current categories in database
+   * @returns 
+   */
+  getProductCategories(): Observable<ProductCategory[]> {
     return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
       map(response => response._embedded.productCategory)
     );
   }
 
+  /**
+   * Searches for items based on keyword typed by user
+   * @param keyword 
+   * @returns 
+   */
   searchProducts(keyword: string) {
     const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${keyword}`
     return this.getProducts(searchUrl);
   }
 
+  /**
+   * Grabs details of a single product when clicked on.
+   * @param productId 
+   * @returns 
+   */
+  getProduct(productId: number): Observable<Product> {
+    const productUrl = `${this.baseUrl}/${productId}`;
+    return this.httpClient.get<Product>(productUrl);
+  }
+
+  /**
+   * Method for searching specific groups of products.
+   * @param searchUrl
+   * @returns 
+   */
   private getProducts(searchUrl: string) {
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
