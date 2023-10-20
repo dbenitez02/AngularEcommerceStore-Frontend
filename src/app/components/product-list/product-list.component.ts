01f47ccc4187beb49721beb3def0b1382df0c910
dbenitez02 <36527173+D-Benz@users.noreply.github.com>
2023-10-19 21:09:16 -0700

    Added CartService and functionality to the Cart Status component

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -23,7 +25,8 @@ export class ProductListComponent implements OnInit {
   previousKeyword: string ="";
 
   constructor(private productService: ProductService,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private cartService: CartService) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(() => {
@@ -106,6 +109,10 @@ export class ProductListComponent implements OnInit {
   }
 
   addToCart(product: Product) {
-    console.log(`Adding to cart: ${product.name}: ${product.unitPrice}`)
+    console.log(`Adding to cart: ${product.name}: ${product.unitPrice}`);
+
+    const cartItem = new CartItem(product);
+
+    this.cartService.addToCart(cartItem);
   }
 }
---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { CartItem } from '../common/cart-item';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  totalCartItems: CartItem[] = [];
+
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
+
+  constructor() { }
+
+  addToCart(cartItem: CartItem) {
+
+    let alreadyExistInCart: boolean = false;
+    let existingCartItem: CartItem = undefined!;
+
+    if (this.totalCartItems.length > 0) {
+
+      for (let tempCartItem of this.totalCartItems) {
+
+        if (tempCartItem.id === cartItem.id) {
+          existingCartItem = tempCartItem;
+          break
+        }
+      }
+
+      alreadyExistInCart = (existingCartItem != undefined);
+    }
+
+    if (alreadyExistInCart) {
+      existingCartItem.quantity++;
+    }
+    else {
+      this.totalCartItems.push(cartItem);
+    }
+
+    this.computeCartTotal(this.totalCartItems);
+  }
+
+  computeCartTotal(cartItems: CartItem[]) {
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+
+    for (let currentCartItem of cartItems) {
+      totalPriceValue += currentCartItem.quantity * currentCartItem.unitPrice;
+      totalQuantityValue += currentCartItem.quantity;
+    }
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+
+    // Debugging purposes
+    console.log(`Contents`);
+    for (let tempItem of this.totalCartItems) {
+      console.log(`Name: ${tempItem.name}\nQuantity: ${tempItem.quantity}\nPrice: ${tempItem.unitPrice}\n-----`)
+    }
+    console.log(`totalPrice: ${totalPriceValue.toFixed(2)}\ntotalQuantity: ${totalQuantityValue}}`);
+  }
+}
