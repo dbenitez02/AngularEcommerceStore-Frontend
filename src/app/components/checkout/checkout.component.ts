ccb445c0cf559d115f14f1d0073292647c08bd6a
dbenitez02 <36527173+D-Benz@users.noreply.github.com>
2023-11-05 13:15:13 -0800

    added custom validator to detect white space

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
+import { CustomValidators } from 'src/app/custromValidators/CustomValidators';
 import { FormService } from 'src/app/services/form.service';
 
 @Component({
@@ -29,10 +30,10 @@ export class CheckoutComponent implements OnInit {
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
-        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2), CustomValidators.notOnlyWhiteSpace]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2), CustomValidators.notOnlyWhiteSpace]),
         email: new FormControl('',
-          [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,6}$')])
+          [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,6}$'), CustomValidators.notOnlyWhiteSpace])
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
---
+++ src/app/custromValidators/validators.spec.ts
@@ -0,0 +1,7 @@
+import { CustomValidators } from './CustomValidators';
+
+describe('Validators', () => {
+  it('should create an instance', () => {
+    expect(new CustomValidators()).toBeTruthy();
+  });
+});
